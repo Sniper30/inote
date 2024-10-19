7eb966b5f741d1ec4d14da027853bcf11411c9a9
Kelvin Peña <sniper-lavoz@hotmail.com>
2024-10-19 13:49:00 -0400

    adding login and register

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,32 +1,23 @@
 import { redirect } from "next/navigation";
 import { createClient } from "../utils/supabase/serverSupabase"
 import { revalidatePath } from "next/cache";
+import Form from "./form";
+import Link from "next/link";
+import { inter } from "../fonts";
 
-
-
-async function loginAction(formData: FormData){
-    "use server"
-
-    const data = {
-        email :  formData.get('email') as string,
-        password: formData.get('password') as string
-    }
-
-    const supabase_client = createClient();
-    const {error} = await supabase_client.auth.signInWithPassword(data);
-    if(error) redirect('/error');
-
-    revalidatePath('/','layout');
-    redirect('/');
-}
 
 export default async function LoginPage(){
 
     return (
-        <form className="text-black">
-            <input  type="text" name="email" required/>
-            <input type="text" name="password" />
-            <button formAction={loginAction} className="text-white">login</button>
-        </form>
+        <div className={`${inter.className} bg-zinc-900 flex flex-col items-center justify-center h-full `}>
+            <div className="bg-zinc-700 flex flex-col items-start  gap-6 w-1/4 h-fit p-6  justify-center  rounded-lg border-zinc-500 border-[1px] text-zinc-100">
+            <h1 className="font-light text-2xl">Sign in to your account</h1>
+            <Form />
+            <Link 
+                href={'/register'}
+            >i dont have an account</Link>
+
+            </div>
+        </div>
     )
 }
