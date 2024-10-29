d08d1a300873756fd6cb2ac5c710668e1fde1d9e
Kelvin Peña <sniper-lavoz@hotmail.com>
2024-10-29 19:34:22 -0400

    fixing responsive

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -3,12 +3,13 @@ import Link from "next/link";
 
 export default function ErrorPage(){
     return (
-        <div className="h-full w-full flex justify-center items-center bg-zinc-800 antialiased">
-        <div className="p-5 bg-yellow-700  w-2/4 h-1/4 rounded-lg">
-        <div className="text-balance text-xl w-full">
-                <p>An error happened make shure you verify your email, if that 
+        <div className="h-full w-full flex flex-col justify-center items-center bg-zinc-800 antialiased lg:pb-72">
+            <h3 className=" w-3/12 p-5 text-3xl">Error</h3>
+        <div className="p-3 bg-yellow-700 w-full sm:w-9/12  xl:w-5/12 rounded-lg min-h-40 h-fit">
+        <div className="text-balance text-xl w-full h-fit flex flex-col justify-center items-center text-center ">
+                <p className="w-full">An error happened make shure you verify your email, if that 
                     the case go to your email account and look for a email by kelvin rafael </p>
-                <p >go to <Link className="font-black" href={'/login'}>log in</Link></p>
+                <p className="w-full" >go to <Link className="font-black" href={'/login'}>log in</Link></p>
         </div>
         </div>
     </div>
--- app/validate/page.tsx
+++ app/validate/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function EmailVerificationPage() {
     return (
     <div className="h-full w-full flex justify-center items-center bg-zinc-800 antialiased">
-        <div className="p-5 bg-yellow-700  w-2/4 h-1/4 rounded-lg">
-        <div className="text-balance text-xl w-full">
+        <div className="p-3 bg-yellow-700 w-full sm:w-9/12  xl:w-5/12 rounded-lg min-h-40 h-fit">
+        <div className="text-balance text-xl w-full h-fit flex flex-col justify-center items-center text-center ">
                 <p>Now you can go to your email account and click on the link that our team send you. </p>
                 <p>That link verify that its a valid email and its your email. </p>
                 <br />
